170da3f24673b2fd8816f68b8efeb8ad142e7cb1
mariano <m_villa@hotmail.com>
2017-06-08 22:18:53 -0300

    Tab + fragments

---
+++ app/src/main/java/com/z/tabbedactivitytest/Fragment1.java
@@ -0,0 +1,20 @@
+package com.z.tabbedactivitytest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mariano on 08/06/2017.
+ */
+
+public class Fragment1 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment1, container, false);
+        return rootView;
+    }
+
+}
